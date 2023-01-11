6994d9879b4b6e19460b52952da733ffaacc3daa
tekater <tekateplay1@yandex.ru>
2023-01-11 21:29:32 +0900

    Initial commit

---
+++ src/org/example/user/Main.java
@@ -0,0 +1,7 @@
+package org.example.user;
+import com.example.username.User;
+public class Main {
+    public static void main(String[] args) {
+        User userCom = new User(2, 3);
+    }
+}
---
+++ src/org/example/username/User.java
@@ -0,0 +1,13 @@
+package org.example.username;
+
+public class User {
+    int x;
+    int y;
+    public User(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int sum() {
+        return this.x + this.y;
+    }
+}
